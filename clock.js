d1969659738fe9876ae196a0a80fbdb9e7fd2b07
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-07-06 16:12:37 +0900

    I rebuilt the HTML and CSS of the clock from 1. Also, we added a function to output id = set_clock element, getTime function to JavaScript.

---
+++ clock.js
@@ -0,0 +1,35 @@
+setInterval(clock, 1000);
+setInterval(showTime, 1000);
+
+function clock() {
+    var weeks = new Array("Sun", "Mon", "Thu", "Wed", "Thr", "Fri", "Sat"); //曜日を出力
+    var now = new Date();
+    var y = now.getFullYear();
+    var mo = now.getMonth() + 1;
+    var d = now.getDate();
+    var w = weeks[now.getDay()];
+    var h = now.getHours();
+    var mi = now.getMinutes();
+    var s = now.getSeconds();
+
+    if (mo < 10) mo = "0" + mo;
+    if (d < 10) d = "0" + d;
+    if (mi < 10) mi = "0" + mi;
+    if (s < 10) s = "0" + s;
+
+    document.getElementById("clock_date").innerHTML = y + "/" + mo + "/" + d + " (" + w + ")";
+    document.getElementById("clock_time").innerHTML = h + ":" + mi + ":" + s;
+    document.getElementById("clock_frame").style.fontSize = window.innerWidth / 10 + "px";
+}
+
+function showTime() {
+    //var get_time = document.getElementById('clock_time'); // HTML要素オブジェクトを取得
+    // console.log(get_time); //[object HTMLParagraphElement]  要素全てをログ出力？
+
+    var get_time = document.getElementById('clock_time').innerHTML;
+    console.log(get_time); //時間のみ出力
+
+    if (get_time === "16:09:10") {
+        console.log("OH!!!");
+    }
+}
