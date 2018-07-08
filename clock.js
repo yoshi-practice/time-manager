cbe6e8b9b7175ccbd24f33c68060404c317d583e
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-07-08 19:24:53 +0900

    Fixed where break; was not added to Switch statement. Also added a program for each time.

--- clock.js
+++ clock.js
@@ -47,7 +47,6 @@ function clock() {
 
     document.getElementById("clock_date").innerHTML = y + "/" + mo + "/" + d + " (" + w + ")";
     document.getElementById("clock_time").innerHTML = h + ":" + mi + ":" + s;
-    //document.getElementById("schedule").innerHTML = date;
     //document.getElementById("clock_date").innerHTML = date;
     //document.getElementById("clock_time").innerHTML = time;
     document.getElementById("clock_frame").style.fontSize = window.innerWidth / 10 + "px";
@@ -68,61 +67,107 @@ function clock() {
     console.log(get_time); //時間のみ出力
 
 
+    function alart() {
+        document.getElementById("Sound").play();
+        break;
+
+    }
+
     if (dayName === "Sun" || dayName === "Sat") { // もし土曜日もしくは日曜日でないなら
         console.log("休日");
 
         // チャイムを作動させない
     } else {
         console.log("平日");
+
         switch (time) {
             case "09:30:00": //0限START
                 document.getElementById("Sound").play();
                 alert("0限目が始まりました。");
+                break;
+
             case "09:45:00": //0限END・1限START
                 document.getElementById("Sound").play();
                 alert("1限目が始めりました。");
+                break;
+
             case "10:35:00": //1限END・休み時間START
                 document.getElementById("Sound").play();
                 alert("1限目が終わりました。");
+                break;
+
             case "10:45:00": //休み時間END・2限START
                 document.getElementById("Sound").play();
                 alert("2限目が始まりました。");
+                break;
+
             case "11:35:00": //2限END・休み時間START
                 document.getElementById("Sound").play();
                 alert("2限目が終わりました。");
+                break;
+
             case "11:45:00": //休み時間END・3限START
                 document.getElementById("Sound").play();
                 alert("3限目が始まりました。");
+                break;
+
             case "12:35:00": //3限END・昼休みSTART
                 document.getElementById("Sound").play();
                 alert("3限目が終わりました。");
+                break;
+
             case "13:15:00": //昼休みEND・4限START
                 document.getElementById("Sound").play();
                 alert("4限目が始まりました。");
+                break;
+
             case "14:05:00": //4限END・休み時間START
                 document.getElementById("Sound").play();
                 alert("4限目が終わりました。");
+                break;
+
             case "14:15:00": //休み時間END・5限START
                 document.getElementById("Sound").play();
                 alert("5限目が始まりました。");
+                break;
+
             case "15:05:00": //5限END・休み時間START
                 document.getElementById("Sound").play();
                 alert("5限目が終わりました。");
+                break;
+
             case "15:15:00": //休み時間END・6限START
                 document.getElementById("Sound").play();
                 alert("6限目が始まりました。");
+
+                break;
+
             case "16:05:00": //6限END・終礼など
                 document.getElementById("Sound").play();
                 alert("6限目が終わりました。");
+
+                break;
+
             case "16:15:00": //休み時間END・7限START
                 document.getElementById("Sound").play();
                 alert("7限目が始まりました。");
+
+                break;
+
             case "17:05:00": //7限END
                 document.getElementById("Sound").play();
                 alert("7限目が終わりました。");
+
+                break;
+
             case "17:50:00": //下校の促し
+
                 document.getElementById("Sound").play();
                 alert("下校する時間の10分前になりました。");
+
+
+                break;
+
             case "18:00:00": //完全下校
                 document.getElementById("Sound").play();
                 alert("下校する時間になりました。");
